cf2871ba21f5c6be02d3edd19b3da25a10418b79
Motta <muataz.cs@gmail.com>
2021-06-26 23:46:38 +0300

    .

---
+++ src/com/exercises/chapter01/MainDriver.java
@@ -0,0 +1,19 @@
+package com.exercises.chapter01;
+
+public class MainDriver {
+	
+	public static void main(String[] args) {
+		
+
+		printHello();
+		
+	}
+	
+	public static void printHello() {
+		
+		
+		System.out.println("hello");
+		
+	}
+
+}
