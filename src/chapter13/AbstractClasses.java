7a892da176018c0f74f8298fc330a112049544df
Motta <muataz.cs@gmail.com>
2021-06-26 22:29:39 +0300

    first commit

---
+++ src/chapter13/AbstractClasses.java
@@ -0,0 +1,13 @@
+package chapter13;
+
+public class AbstractClasses {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	 System.out.println("hello");
+	 System.out.println("0");
+
+	}
+
+}
