c89f6b20ed36fd8098d8bb4362fd5e84f55accab
Motta <muataz.cs@gmail.com>
2021-07-02 12:50:02 +0300

    .

--- src/com/chapter13/listings/listing13/TestGeomatricObject.java
+++ src/com/chapter13/listings/listing13/TestGeomatricObject.java
@@ -7,7 +7,24 @@ public class TestGeomatricObject {
 		
 		GeometricObject geoObject1 = new Circle(5);
 		GeometricObject geoObject2 = new Rectangle(5, 3);
+		
+		System.out.println("The two objects has the same area? " + equalArea(geoObject1, geoObject2));
 
+		
+	}
+	
+	public static boolean equalArea(GeometricObject object1, GeometricObject object2) {
+		
+		
+		return object1.getArea() == object2.getArea();
+	}
+	
+	public static void displayGeomatricObject(GeometricObject object) {
+		
+		System.out.println();
+		System.out.println("The area is " + object.getArea());
+		System.out.println("The perimeter is " + object.getPerimeter());
+		
 		
 	}
 
